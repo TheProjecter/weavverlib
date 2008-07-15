311ef7f351aec596ed07c597119ada69104be52b
mitcheloc <mitcheloc@80b3114d-0252-0410-8ff7-432fba7e9de9>
2008-07-15 17:19:27 +0000

    yay, first commit!
    i wonder how this will turn out :)

---
+++ Installer/InstallerBase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WeavverLib.Installer
+{
+     public class InstallerBase
+     {
+          public virtual void Install()
+          {
+          }
+
+          public virtual void Uninstall()
+          {
+          }
+     }
+}
---
+++ Sockets/AsteriskClient.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WeavverLib
+{
+     public class AsteriskClient
+     {
+     }
+}
