3c051864d21b80a99343e11af6d94011dae38d5b
mitcheloc <mitcheloc@80b3114d-0252-0410-8ff7-432fba7e9de9>
2008-07-16 03:27:03 +0000

    Made good progress on the installation framework ;)

---
+++ Application Framework/WeavverApplication.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WeavverLib.Application_Framework
+{
+     public class WeavverApplication : System.Windows.Forms.ApplicationContext
+     {
+          public WeavverApplication()
+          {
+          }
+     }
+}
